440c0176f5c65cf3ee23b9ab7bdbb7da53e05525
Sammy Pfeiffer <sammypfeiffer@gmail.com>
2015-06-28 21:51:43 +0200

    Now we can test also the batterystatus way of using the node

--- scripts/test_plugin.py
+++ scripts/test_plugin.py
@@ -33,44 +33,60 @@ import rospy
 from std_msgs.msg import Float32, Bool
 import random
 
+TEST_PARAM_VERSION = False
 
-# rosparam set /batteries_dashboard/batteries "{'battery1': {'percentage_topic': '/percentage1', 'charging_topic': '/charging1', 'tooltip_name': 'BATT1'}}"
-# rosparam set /batteries_dashboard/batteries "{'battery2': {'percentage_topic': '/percentage2', 'charging_topic': '/charging2', 'tooltip_name': 'BATT2'}}"
-# rosparam set /batteries_dashboard/emergency_buttons "{'emergency1': {'pressed_topic': '/emergency1', 'tooltip_name': 'EMERGENCY1'}}"
+if TEST_PARAM_VERSION:
+    # rosparam set /batteries_dashboard/batteries "{'battery1': {'percentage_topic': '/percentage1', 'charging_topic': '/charging1', 'tooltip_name': 'BATT1'}}"
+    # rosparam set /batteries_dashboard/batteries "{'battery2': {'percentage_topic': '/percentage2', 'charging_topic': '/charging2', 'tooltip_name': 'BATT2'}}"
+    # rosparam set /batteries_dashboard/emergency_buttons "{'emergency1': {'pressed_topic': '/emergency1', 'tooltip_name': 'EMERGENCY1'}}"
 
-NUM_BATTERIES = 4
-NUM_EMERGENCY_B = 2
+    NUM_BATTERIES = 4
+    NUM_EMERGENCY_B = 2
 
-rospy.init_node('test_rqt_battery')
-batteries_list = []
-for i in range(NUM_BATTERIES):
-    batteries_list.append({'battery'+str(i) : {'percentage_topic': '/percentage'+str(i), 'charging_topic': '/charging'+str(i), 'tooltip_name': 'BATT'+str(i)}})
+    rospy.init_node('test_rqt_battery')
+    batteries_list = []
+    for i in range(NUM_BATTERIES):
+        batteries_list.append({'battery'+str(i) : {'percentage_topic': '/percentage'+str(i), 'charging_topic': '/charging'+str(i), 'tooltip_name': 'BATT'+str(i)}})
 
 
-rospy.loginfo("Setting params.")
-rospy.set_param('/batteries_dashboard/batteries', batteries_list)
-own_params = rospy.get_param('/batteries_dashboard')
-rospy.loginfo("We set the params:\n" + str(own_params))
+    rospy.loginfo("Setting params.")
+    rospy.set_param('/batteries_dashboard/batteries', batteries_list)
+    own_params = rospy.get_param('/batteries_dashboard')
+    rospy.loginfo("We set the params:\n" + str(own_params))
 
-for battery_list_elem in batteries_list:
-    for battery in battery_list_elem.keys():
-        battery_list_elem[battery].update({'percentage_pub': rospy.Publisher(battery_list_elem[battery]['percentage_topic'], Float32)})
-        battery_list_elem[battery].update({'charging_pub': rospy.Publisher(battery_list_elem[battery]['charging_topic'], Bool)})
+    for battery_list_elem in batteries_list:
+        for battery in battery_list_elem.keys():
+            battery_list_elem[battery].update({'percentage_pub': rospy.Publisher(battery_list_elem[battery]['percentage_topic'], Float32)})
+            battery_list_elem[battery].update({'charging_pub': rospy.Publisher(battery_list_elem[battery]['charging_topic'], Bool)})
 
-print "batteries_list looks like:\n" + str(batteries_list)
+    print "batteries_list looks like:\n" + str(batteries_list)
 
-battery_status = [i for i in range(1, 100, 10)]
-battery_status.reverse()
+    battery_status = [i for i in range(1, 100, 10)]
+    battery_status.reverse()
 
-hz = 50.0
+    hz = 50.0
 
-# Publish battery levels going down simulating a robot
-while not rospy.is_shutdown():
-    for status in battery_status:
-        rospy.loginfo("Publishing status: " + str(status))
-        for i in range(int(hz)): # One second with every status at hz
-            for battery_list_elem in batteries_list:
-                for battery in battery_list_elem.keys():
-                    battery_list_elem[battery]['percentage_pub'].publish(status)
-                    battery_list_elem[battery]['charging_pub'].publish(random.choice([True, False]))
-            rospy.sleep(1.0/hz)
+    # Publish battery levels going down simulating a robot
+    while not rospy.is_shutdown():
+        for status in battery_status:
+            rospy.loginfo("Publishing status: " + str(status))
+            for i in range(int(hz)): # One second with every status at hz
+                for battery_list_elem in batteries_list:
+                    for battery in battery_list_elem.keys():
+                        battery_list_elem[battery]['percentage_pub'].publish(status)
+                        battery_list_elem[battery]['charging_pub'].publish(random.choice([True, False]))
+                rospy.sleep(1.0/hz)
+
+else:
+    rospy.init_node('test_rqt_battery')
+    from power_msgs.msg import BatteryStatus
+    pub = rospy.Publisher('/battery_status', BatteryStatus, queue_size=1)
+
+    while not rospy.is_shutdown():
+        for i in range(0, 100, 10):
+            bs = BatteryStatus()
+            bs.percentage = float(i)
+            bs.charging = random.choice([True, False])
+            rospy.loginfo("Publishing: " + str(bs))
+            pub.publish(bs)
+            rospy.sleep(1.0)
